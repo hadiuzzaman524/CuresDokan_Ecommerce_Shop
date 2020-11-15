c20f71083923a0c3b151ccfd190dfaf9160305a9
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-11-15 21:59:31 +0600

    update provider version

--- curesdokan/lib/main.dart
+++ curesdokan/lib/main.dart
@@ -5,33 +5,32 @@ import './screens/home_screen.dart';
 import './screens/details_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => Products(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => Products(),
-
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Colors.purple,
-          accentColor: Colors.purpleAccent,
-          textTheme: TextTheme(
-            headline1: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        accentColor: Colors.purpleAccent,
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        initialRoute: HomeScreen.routeName,
-        routes: {
-          HomeScreen.routeName: (context) => HomeScreen(),
-          DetailsScreen.routeName: (context) => DetailsScreen(),
-        },
       ),
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        DetailsScreen.routeName: (context) => DetailsScreen(),
+      },
     );
   }
 }
