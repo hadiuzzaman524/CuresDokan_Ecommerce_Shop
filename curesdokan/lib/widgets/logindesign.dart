85270f529ba3b38ceaebc33ae6eda66dd320259d
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-12-09 13:31:41 +0600

    add form validation functionality

--- curesdokan/lib/widgets/logindesign.dart
+++ curesdokan/lib/widgets/logindesign.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
-class LoginDesign extends StatelessWidget {
+class LoginDesign extends StatefulWidget {
+  @override
+  _LoginDesignState createState() => _LoginDesignState();
+}
+
+class _LoginDesignState extends State<LoginDesign> {
+  final _formKey = GlobalKey<FormState>();
+  var _passwordFocus = FocusNode();
+
+  String _email;
+  String _password;
+
+  _saveData() {
+    _formKey.currentState.save();
+    bool isValid = _formKey.currentState.validate();
+    if (isValid) {
+      // valid email and password here
+      print(_email);
+      print(_password);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: Form(
+        key: _formKey,
         child: Column(
           children: [
             TextFormField(
@@ -13,18 +35,51 @@ class LoginDesign extends StatelessWidget {
               decoration: InputDecoration(
                 labelText: 'Email',
               ),
+              textInputAction: TextInputAction.next,
+              validator: (value){
+                if (value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                if ((!value.endsWith('@gmail.com'))) {
+                  return 'Please enter valid email';
+                }
+                return null;
+              },
+              onFieldSubmitted: (_){
+                FocusScope.of(context).requestFocus(_passwordFocus);
+              },
+              onSaved: (value){
+                _email=value;
+              },
             ),
             TextFormField(
               keyboardType: TextInputType.visiblePassword,
               decoration: InputDecoration(
                 labelText: 'Password',
               ),
+              focusNode: _passwordFocus,
+              validator: (value){
+                if(value.isEmpty){
+                  return 'Please enter password';
+                }
+                if(value.length<6){
+                  return 'Password must be in 6 character long';
+                }
+                return null;
+              },
+              onSaved: (value){
+                _password=value;
+              },
+              onFieldSubmitted: (_){
+                _saveData();
+              },
             ),
             Container(
-              margin: EdgeInsets.only(top: 30,bottom: 50),
+              margin: EdgeInsets.only(top: 30, bottom: 50),
               child: GestureDetector(
                 onTap: () {
                   //
+                  _saveData();
                 },
                 child: Container(
                   width: double.infinity,
--- curesdokan/lib/widgets/signupdesign.dart
+++ curesdokan/lib/widgets/signupdesign.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class SignUpDesign extends StatelessWidget {
+class SignUpDesign extends StatefulWidget {
+  @override
+  _SignUpDesignState createState() => _SignUpDesignState();
+}
+
+class _SignUpDesignState extends State<SignUpDesign> {
+  final formKey = GlobalKey<FormState>();
+  var _emailFocus=FocusNode();
+  var _passwordFocus=FocusNode();
+  var _confirmFocus=FocusNode();
+
+  String _email;
+  String _password;
+  String _confirmPassword;
+
+
+  _saveData(){
+    formKey.currentState.save();
+    bool isValid=formKey.currentState.validate();
+    if(isValid){
+      // valid email and password here
+      print(_email);
+      print(_password);
+
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         child: Form(
+          key: formKey,
           child: Column(
             children: [
               TextFormField(
@@ -14,24 +41,78 @@ class SignUpDesign extends StatelessWidget {
                 decoration: InputDecoration(
                   labelText: 'Email',
                 ),
+                textInputAction: TextInputAction.next,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if ((!value.endsWith('@gmail.com'))) {
+                    return 'Please enter valid email';
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  _email=value;
+                },
+                onFieldSubmitted: (_){
+                  FocusScope.of(context).requestFocus(_passwordFocus);
+                },
               ),
               TextFormField(
                 keyboardType: TextInputType.visiblePassword,
                 decoration: InputDecoration(
                   labelText: 'Password',
                 ),
+                textInputAction: TextInputAction.next,
+                focusNode: _passwordFocus,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be 6 character long';
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  _password=value;
+                },
+                onFieldSubmitted: (_){
+                  FocusScope.of(context).requestFocus(_confirmFocus);
+                },
               ),
               TextFormField(
-                keyboardType: TextInputType.visiblePassword,
+                keyboardType: TextInputType.text,
                 decoration: InputDecoration(
                   labelText: 'Confirm Password',
                 ),
+                textInputAction: TextInputAction.done,
+                focusNode: _confirmFocus,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be 6 character long';
+                  }
+                  if(_password!=value){
+                    return 'Your password can\'t match';
+                  }
+                  return null;
+                },
+                onSaved: (value){
+                  _confirmPassword=value;
+                },
+                onFieldSubmitted: (_){
+                  _saveData();
+                },
               ),
               Container(
-                margin: EdgeInsets.only(top: 30,bottom: 50),
+                margin: EdgeInsets.only(top: 30, bottom: 50),
                 child: GestureDetector(
                   onTap: () {
                     //
+                    _saveData();
                   },
                   child: Container(
                     width: double.infinity,
